be3ee203c0058f7e9998a857cfdcc3d3e76cf909
Jose Maria Ocana <telecomu4@gmail.com>
2014-09-18 22:05:48 +0200

    background funcionality mobile

--- js/main.js
+++ js/main.js
@@ -9,29 +9,43 @@ $( document ).ready(function() { //DOM OK!
 	});
 
 	//backgrounds search box
-	//=================================
-	//CONFIG IMAGES
-	//=================================
+	
 	$(function() {
+		//=================================
+		//CONFIG IMAGES
+		//=================================
 		 var images = ['/fanontour/img/backgrounds/home/tickets_h_600px.jpg','/fanontour/img/backgrounds/home/hotels_h_600px.jpg', '/fanontour/img/backgrounds/home/recorte_h_600px.jpg','/fanontour/img/backgrounds/home/recorte_h_600px.jpg','/fanontour/img/backgrounds/home/recorte_h_600px.jpg','/fanontour/img/backgrounds/home/recorte_h_600px.jpg','/fanontour/img/backgrounds/home/recorte_h_600px.jpg'];
-		 var d = document.createElement("div");
-		 d.style.display = 'none';
-		 document.body.appendChild(d);
-		 for (var i in images) 
-		 {
-		   var img = document.createElement("img");
-		   img.src = images[i];
-		   d.appendChild(img);
-		 }
+		//================================
+		var d = document.createElement("div");
+		d.style.display = 'none';
+		document.body.appendChild(d);
+		for (var i in images) 
+		{
+		  var img = document.createElement("img");
+		  img.src = images[i];
+		  d.appendChild(img);
+		}
 
 
-		 $( "#fanontour-search-box .nav a" ).click(function() {
-		 	var num_sec = $(this).parent().prevAll().length;
-		  	$(".bg-container").fadeOut("fast", function(){		  				  
-		  		$(".bg-container").css("background","url('"+ images[num_sec] +"')no-repeat 0 0");
-		  		$(".bg-container").css("background-size","contain");
-		  		$(this).fadeIn("fast");
-		  	})
+		$( "#fanontour-search-box .nav a" ).click(function() {
+			//=================================
+			//CONFIG FUNCIONALITY BACKGROUNDS
+			//=================================
+			var search_box_width = 922;
+			//=================================
+		 	var device_width = $(window).width();
+
+		 	if(device_width >= search_box_width){
+			 	var num_sec = $(this).parent().prevAll().length;
+			  	$(".bg-container").fadeOut("fast", function(){		  				  
+			  		$(".bg-container").css("background","url('"+ images[num_sec] +"')no-repeat 0 0");
+			  		$(".bg-container").css("background-size","cover");
+			  		$(this).fadeIn("fast");
+			  	})
+			}  	
+			else{
+				event.preventdefault();
+			}  	
 		  	
 		});
 	});
