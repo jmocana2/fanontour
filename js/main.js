3e11ac40e1d2e96377335063ae30a9de2526f82f
Jose Maria Ocana <telecomu4@gmail.com>
2014-11-06 19:34:36 +0100

    added flight results

--- js/main.js
+++ js/main.js
@@ -560,90 +560,164 @@ $( document ).ready(function() { //DOM OK!
 	//SLIDER FORM
 	//=======================
 	if($(".results").length){
-		//************* CONFIG ***************
-		var min_duration = 0;
-		var max_duration = 120;
-		var min_price = 0;
-		var max_price = 600;
+
+		if($("#activities").length){
+			//=========== ACTIVITIES =============
+			//************* CONFIG ***************
+			var min_duration = 0;
+			var max_duration = 120;
+			var min_price = 0;
+			var max_price = 600;
+			var itemsPage = 5;
 
 
-		$('#price-slider, #duration-slider').slider()
+			$('#activities #price-slider,#activities #duration-slider').slider()
 
-		function filterActivities(v1min, v1max, v2min, v2max){
+			function filterActivities(v1min, v1max, v2min, v2max){
 
-			$(".results ul").remove();
-		  	var resultHtml = "<ul id='list_results'>";
+				$(".results ul").remove();
+			  	var resultHtml = "<ul id='list_results'>";
 
-		    $.get("/fanontour/xml/results/activity.xml", function (xml) {
-			    $(xml).find("activity").each(function () {	
+			    $.get("/fanontour/xml/results/activity.xml", function (xml) {
+				    $(xml).find("activity").each(function () {	
 
-			       var val_price = parseInt($(this).find('price').text());
-			       var val_duration = parseInt($(this).find('duration').text());
-			       if(isNaN(val_duration)){
-			       	val_duration = max_duration;
-			       }
-			      
-			       if((val_price >= v1min && val_price <= v1max) && (val_duration >= v2min && val_duration <= v2max)){
-			       	   var name = $(this).find('name').text();
-	       			   var description = $(this).find('description').text();
-	       			   var image = $(this).find('image').text();
-	       			   var price = $(this).find('price').text(); 
-	       			   var duration = $(this).find('duration').text();     			   
-	       			   var source = $(this).find('source').text();
-	       			   var url = $(this).find('url').text();
-				       
-				       resultHtml += "<li class='mod-result'><div class='mod-img'><img src="+ image +" alt=" + name +"></div>";
-				       resultHtml += "<div class='mod-txt'><h2>"+ name +"</h2><p>"+ description +"</p></div>";
-				       resultHtml += "<div class='result-footer'><ul class='list-inline'><li class='price' data-value="+ price + ">" + price +" &euro;</li><li class='source'><a class='btn btn-search' href='"+ url +"'>Book here</a></li><li class='duration' data-value='"+ val_duration +"'><span class='fanontour-icon icon-icons-fanontour_clock'></span> "+ duration +" <abbr title='approximately'> approx.</abbr></li><li><span class='fanontour-icon icon-icons-fanontour_source'></span> Found in <strong>"+ source +"</strong></li></div>";			
-			       }			      
-			    });
+				       var val_price = parseInt($(this).find('price').text());
+				       var val_duration = parseInt($(this).find('duration').text());
+				       if(isNaN(val_duration)){
+				       	val_duration = max_duration;
+				       }
+				      
+				       if((val_price >= v1min && val_price <= v1max) && (val_duration >= v2min && val_duration <= v2max)){
+				       	   var name = $(this).find('name').text();
+		       			   var description = $(this).find('description').text();
+		       			   var image = $(this).find('image').text();
+		       			   var price = $(this).find('price').text(); 
+		       			   var duration = $(this).find('duration').text();     			   
+		       			   var source = $(this).find('source').text();
+		       			   var url = $(this).find('url').text();
+					       
+					       resultHtml += "<li class='mod-result'><div class='mod-img'><img src="+ image +" alt=" + name +"></div>";
+					       resultHtml += "<div class='mod-txt'><h2>"+ name +"</h2><p>"+ description +"</p></div>";
+					       resultHtml += "<div class='result-footer'><ul class='list-inline'><li class='price' data-value="+ price + ">" + price +" &euro;</li><li class='source'><a class='btn btn-search' href='"+ url +"'>Book here</a></li><li class='duration' data-value='"+ val_duration +"'><span class='fanontour-icon icon-icons-fanontour_clock'></span> "+ duration +" <abbr title='approximately'> approx.</abbr></li><li><span class='fanontour-icon icon-icons-fanontour_source'></span> Found in <strong>"+ source +"</strong></li></div>";			
+				       }			      
+				    });
 
-				resultHtml += "</ul>";
-				$(".results").html(resultHtml).promise().done(function(){
-       				orderFanontour($("#sort-field-01").attr("data-value"));
-       				paginarFanontour($("#items-field-01").val()); 
-       				setResults();      				       				
-    			});
+					resultHtml += "</ul>";
+					$(".results").html(resultHtml).promise().done(function(){
+	       				orderFanontour($("#sort-field-01").attr("data-value"));
+	       				paginarFanontour($("#items-field-01").val()); 
+	       				setResults();      				       				
+	    			});
+				});
+			}
+
+			//SLIDER
+			$('#activities #price-slider, #activities #duration-slider').slider()
+			  .on('slideStop', function(ev){
+
+			  	//Get min-max values
+			  	//Min max prices
+			  	if($("#price-slider").val() == ""){
+			  		var interval = min_price + "," + max_price;
+			  	}else{
+			  		var interval = $("#price-slider").val();
+			  	}
+
+			  	if($("#duration-slider").val() == ""){
+			  		var interval2 = min_duration + "," +  max_duration;
+			  	}else{
+			  		var interval2 = $("#duration-slider").val();
+			  	}
+			    
+			    var res = interval.split(",");
+			    var v1min = res[0];
+			    var v1max = res[1];
+			    //Min max duration
+			 
+			    var res2 = interval2.split(",");
+			    var v2min = res2[0];
+			    var v2max = res2[1];
+
+			    filterActivities(v1min, v1max, v2min, v2max)	  	
 			});
-		}
 
-		//PRICE SLIDER
-		$('#price-slider, #duration-slider').slider()
-		  .on('slideStop', function(ev){
+			//PAGINATION
+			/* initiate plugin */
+		    $("ul.pagination").jPages({
+		      containerID : "list_results",
+		      perPage : itemsPage
+		    });
+	    }  
+		//=========== / ACTIVITIES =============
 
-		  	//Get min-max values
-		  	//Min max prices
-		  	if($("#price-slider").val() == ""){
-		  		var interval = min_price + "," + max_price;
-		  	}else{
-		  		var interval = $("#price-slider").val();
-		  	}
+		//============= FLIGHTS ==============
+		if($("#flights").length){
+			//************* CONFIG ***************
+			var min_price = 0;
+			var max_price = 4000;
+			var itemsPage = 10;
 
-		  	if($("#duration-slider").val() == ""){
-		  		var interval2 = min_duration + "," +  max_duration;
-		  	}else{
-		  		var interval2 = $("#duration-slider").val();
-		  	}
-		    
-		    var res = interval.split(",");
-		    var v1min = res[0];
-		    var v1max = res[1];
-		    //Min max duration
-		 
-		    var res2 = interval2.split(",");
-		    var v2min = res2[0];
-		    var v2max = res2[1];
+			$('#flights #price-slider').slider();
 
-		    filterActivities(v1min, v1max, v2min, v2max)		   
-		  	
-		  });
+			function filterFlights(v1min, v1max){
 
-		  //PAGINATION
-		/* initiate plugin */
-	    $("ul.pagination").jPages({
-	      containerID : "list_results",
-	      perPage : 5
-	    });
+				$(".results ul").remove();
+			  	var resultHtml = "<ul id='list_results'>";
+
+			    $.get("/fanontour/xml/results/flights.xml", function (xml) {
+				    $(xml).find("flight").each(function () {	
+
+				       var val_price = parseInt($(this).find('price').text());
+				      			      
+				       if(val_price >= v1min && val_price <= v1max){
+				       	   var route = $(this).find('route').text();
+		       			   var price = $(this).find('price').text(); 	       			     			   
+		       			   var source = $(this).find('source').text();
+		       			   var url = $(this).find('url').text();
+					       
+					       resultHtml += "<li class='mod-result panel panel-result'>";
+					       resultHtml += "<div class='mod-txt'><h2>"+ route +"</h2></div>";
+					       resultHtml += "<div class='result-footer'><ul class='list-inline'><li class='price' data-value="+ price + ">" + price +" &euro;</li><li class='source'><a class='btn btn-search' href='"+ url +"'>Book here</a></li><li><span class='fanontour-icon icon-icons-fanontour_source'></span> Found in <strong>"+ source +"</strong></li></div>";			
+				       }			      
+				    });
+
+					resultHtml += "</ul>";
+					$(".results").html(resultHtml).promise().done(function(){
+	       				orderFanontour($("#sort-field-01").attr("data-value"));
+	       				paginarFanontour($("#items-field-01").val()); 
+	       				setResults();      				       				
+	    			});
+				});
+			}
+
+			//SLIDER
+			$('#flights #price-slider').slider()
+			  .on('slideStop', function(ev){
+
+			  	//Get min-max values
+			  	//Min max prices
+			  	if($("#price-slider").val() == ""){
+			  		var interval = min_price + "," + max_price;
+			  	}else{
+			  		var interval = $("#price-slider").val();
+			  	}		  	
+			    
+			    var res = interval.split(",");
+			    var v1min = res[0];
+			    var v1max = res[1];		    
+
+			    filterFlights(v1min, v1max);	  	
+			});
+
+			//PAGINATION
+			/* initiate plugin */
+		    $("ul.pagination").jPages({
+		      containerID : "list_results",
+		      perPage : itemsPage
+		    });
+		}			
+
+		
 
 	    function paginarFanontour(num_pag){
 	      /* get new nº of items per page */
